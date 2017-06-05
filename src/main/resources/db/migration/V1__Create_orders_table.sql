a469977ab0058c67bec152834e948500bbdad2e6
BBK-PiJ-2015-87 <ivanovs223@gmail.com>
2017-06-05 17:20:19 +0100

    Add DB migration scripts

---
+++ src/main/resources/db/migration/V1__Create_orders_table.sql
@@ -0,0 +1,4 @@
+create table ORDERS (
+    ID int not null,
+    NAME varchar(100) not null
+);
---
+++ src/main/resources/db/migration/V2__Add_orders.sql
@@ -0,0 +1,3 @@
+insert into ORDERS (ID, NAME) values (1, 'order one');
+insert into ORDERS (ID, NAME) values (2, 'order two');
+insert into ORDERS (ID, NAME) values (3, 'order three');
